a308ffab98991303fce00bd6c94aebbe5e40ebcc
raviprasad <raviprasad93335@gmail.com>
2026-08-28 21:33:40 +0530

    02 dataTypes

---
+++ 01_basic/01_variabals.js
@@ -0,0 +1,10 @@
+const Userid = "01";
+let UserName = "ravi";
+let Pass = "1234";
+//Number="1234567890";
+let kamra;
+
+/*console.log("Userid");
+console.log(UserName);*/
+
+console.table([UserName, Userid, Pass, Number, kamra]);
---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,25 @@
+let name = "ravi"; //string
+let age = 24; // number
+let raviIsMale = true; // boolean
+let toMuchbignumber = 123456789012345678901234567890999999912345678912345678901234567890n;// bigint
+let raviSymbol = Symbol("this is symbol"); // symbol
+let karma; // undefined
+let nullValue = null; // null
+
+//objects
+
+
+
+
+
+
+
+
+
+console.log(typeof name);
+
+
+//console.table([name, age, raviIsMale, toMuchbignumber, raviSymbol]);
+//console.log(3+3); 
+//console.log(3+6)
+
