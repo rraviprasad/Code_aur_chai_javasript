31b4cc7966c5a4e08ffa63f748fb60901b83753a
raviprasad <raviprasad93335@gmail.com>
2026-08-28 23:47:15 +0530

    converstion of dataTypes

---
+++ 01_basic/03_converstion_oprestons.js
@@ -0,0 +1,58 @@
+let score = 33;
+let scoreString = Number(score);
+
+
+// let rank = "3wert";
+
+// let numbervalu = Number(rank);
+// console.log(typeof numbervalu);
+// console.log(numbervalu);
+
+// let rank = "werty";
+
+// let numbervalu = String(rank);
+// console.log(typeof numbervalu);
+// console.log(numbervalu);
+
+// let rank = null;
+
+// let numbervalu = Number(rank);
+// console.log(typeof numbervalu);
+// console.log(numbervalu);
+
+
+// let rank = false;
+
+// let numbervalu = Number(rank);
+// console.log(typeof numbervalu);
+// console.log(numbervalu);
+
+
+// let rank = true;
+
+// let numbervalu = Number(rank);
+// console.log(typeof numbervalu);
+// console.log(numbervalu);
+
+// "33" => 33
+// 33abc => NaN
+// true => 1; false => 0
+
+
+
+// let rank = 1;
+
+// let numbervalu = Boolean(rank);
+// console.log(typeof numbervalu);
+// console.log(numbervalu);
+
+// 1 => true; 0 => false
+// "" => false; "ravi" => true
+
+
+let rank = 15;
+
+let numbervalu = String(rank);
+console.log(typeof numbervalu);
+console.log(numbervalu);
+
